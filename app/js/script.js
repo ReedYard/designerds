138394a55f2b51f768038b572d6787cca02a789c
Reed <mark.nadudvari@gmail.com>
2022-02-03 22:26:42 +0100

    third commit

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,35 @@
-console.log("riido-yaado script.js");
+// _header.scss
+
+// navbar menu opening
+
+const navToggle = document.getElementById("nav-toggle");
+
+navToggle.addEventListener("click", initToggle);
+
+function initToggle() {
+  const nav = document.getElementById("navbar-nav");
+  nav.classList.toggle("open");
+  if (nav.classList.contains("open")) {
+    nav.style.maxHeight = nav.scrollHeight + "px";
+    nav.style.marginTop = "2rem";
+  } else {
+    nav.removeAttribute("style");
+  }
+}
+
+// navbar sticky after scrolling
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("main-nav");
+var nav_backdrop = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > nav_backdrop) {
+    navbar.classList.add("nav-backdrop");
+  } else {
+    navbar.classList.remove("nav-backdrop");
+  }
+}
